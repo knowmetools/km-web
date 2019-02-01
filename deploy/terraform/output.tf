55957f43866f1a36a90dd78a4cad50d710d46da5
Chathan Driehuys <chathan@driehuys.com>
2019-02-01 13:18:35 -0500

    Add codebuild configuration.
    Since the build process is being managed by CodeBuild, we no longer need
    the Terraform code that was used to deploy the application.

--- deploy/terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "bucket" {
-  description = "The name of the S3 bucket that resources are served from."
-  value       = "${aws_s3_bucket.site.id}"
-}
-
-output "domain" {
-  description = "The domain name the provisioned infrastructure is available at."
-  value       = "${local.domain}"
-}
--- deploy/terraform/vars.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" {
-  default     = "us-east-1"
-  description = "The AWS region to provision infrastructure in"
-  type        = "string"
-}
